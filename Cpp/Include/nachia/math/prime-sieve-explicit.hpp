09e038366bf611b2d40104e98a885ef86591b863
Nachia Vivias <piyopiyograph@gol.com>
2022-02-06 16:05:15 +0900

    faster prime sieve

--- Cpp/Include/nachia/math/prime-sieve-explicit.hpp
+++ Cpp/Include/nachia/math/prime-sieve-explicit.hpp
@@ -2,33 +2,34 @@
 #include <vector>
 #include <algorithm>
 #include <cassert>
+#include <iostream>
 
 
 namespace nachia{
 
 namespace prime_sieve_explicit_internal{
-    std::vector<bool> isprime = { false, false };
+    std::vector<bool> isprime = { false }; // a[x] := isprime(2x+1)
 
     void CalcIsPrime(int z){
-        if((int)isprime.size() < z+1){
+        if((int)isprime.size() *2+1 < z+1){
             int new_z = isprime.size();
-            while(new_z < z+1) new_z *= 2;
+            while(new_z*2+1 < z+1) new_z *= 2;
             z = new_z-1;
             isprime.resize(z+1, true);
-            for(int i=2; i*i<=z; i++) if(isprime[i]){
-                for(int j=i*i; j<=z; j+=i) isprime[j] = false;
+            for(int i=1; i*(i+1)*2<=z; i++) if(isprime[i]){
+                for(int j=i*(i+1)*2; j<=z; j+=i*2+1) isprime[j] = false;
             }
         }
     }
     
-    std::vector<int> prime_list;
-    int prime_list_max = 1;
+    std::vector<int> prime_list = {2};
+    int prime_list_max = 0;
 
     void CalcPrimeList(int z){
         while((int)prime_list.size() < z){
-            if((int)isprime.size() == prime_list_max + 1) CalcIsPrime(prime_list_max + 1);
+            if((int)isprime.size() <= prime_list_max + 1) CalcIsPrime(prime_list_max + 1);
             for(int p=prime_list_max+1; p<(int)isprime.size(); p++){
-                if(isprime[p]) prime_list.push_back(p);
+                if(isprime[p]) prime_list.push_back(p*2+1);
             }
             prime_list_max = isprime.size() - 1;
         }
@@ -38,7 +39,7 @@ namespace prime_sieve_explicit_internal{
         if(prime_list_max < z){
             CalcIsPrime(z);
             for(int p=prime_list_max+1; p<(int)isprime.size()-1; p++){
-                if(isprime[p]) prime_list.push_back(p);
+                if(isprime[p]) prime_list.push_back(p*2+1);
             }
             prime_list_max = isprime.size() - 1;
         }
@@ -49,8 +50,10 @@ namespace prime_sieve_explicit_internal{
 
 bool IsprimeExplicit(int n){
     using namespace prime_sieve_explicit_internal;
+    if(n == 2) return true;
+    if(n % 2 == 0) return false;
     CalcIsPrime(n);
-    return isprime[n];
+    return isprime[(n-1)/2];
 }
 
 int NthPrimeExplicit(int n){
@@ -61,13 +64,14 @@ int NthPrimeExplicit(int n){
 
 int PrimeCountingExplicit(int n){
     using namespace prime_sieve_explicit_internal;
+    if(n < 2) return 0;
     CalcPrimeListUntil(n);
     auto res = ::std::upper_bound(prime_list.begin(), prime_list.end(), n) - prime_list.begin();
     return (int)res;
 }
 
 // [l, r)
-::std::vector<bool> SegmentedSieve(long long l, long long r){
+::std::vector<bool> SegmentedSieveExplicit(long long l, long long r){
     assert(0 <= l); assert(l <= r);
     long long d = r - l;
     if(d == 0) return {};
@@ -80,5 +84,6 @@ int PrimeCountingExplicit(int n){
     if(l < 2) for(long long p=l; p<2 && p<r; p++) res[l-p] = false;
     return res;
 }
+
 
 }
