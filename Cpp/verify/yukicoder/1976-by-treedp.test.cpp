b1a48349f724d6a8954bcce82529403b3c77f52b
Nachia Vivias <piyopiyograph@gol.com>
2024-06-03 18:36:02 +0900

    [tree/tree-dp] update verify/yukicoder/1976-by-treedp.test.cpp

--- Cpp/verify/yukicoder/1976-by-treedp.test.cpp
+++ Cpp/verify/yukicoder/1976-by-treedp.test.cpp
@@ -1,5 +1,5 @@
 #define PROBLEM "https://yukicoder.me/problems/no/1976"
-#include "../../Include/nachia/tree/any-direction-tree-dp.hpp"
+#include "../../Include/nachia/tree/tree-dp.hpp"
 #include "../../Include/nachia/misc/fastio.hpp"
 
 int main(){
@@ -15,16 +15,16 @@ int main(){
         int height = 0;
         int diam = 0;
     };
-    auto treedp = nachia::AnyDirectionTreeDP(
+    auto treedp = nachia::TreeDP<Semiclaster>::Solver(
         tree,
-        std::vector<Semiclaster>(N),
-        [](Semiclaster a, Semiclaster b){
+        [](int){ return Semiclaster(); },
+        [](Semiclaster a, Semiclaster b, int){
             Semiclaster res;
             res.height = std::max(a.height, b.height);
             res.diam = std::max({ a.diam, b.diam, a.height + b.height });
             return res;
         },
-        [](Semiclaster a, int egdeIdx, int newRoot){
+        [](Semiclaster a, int, int){
             Semiclaster res;
             res.height = a.height + 1;
             res.diam = std::max(a.diam, res.height);
